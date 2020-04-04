9198c37a4ac7664e1e4bf7e52e0f4d938ddceeb5
VeronikaLapa <Lapshina.nika@yandex.ru>
2020-04-04 16:06:12 +0300

    Updates

---
+++ precache-manifest.49ba32b8552e699621aa000343ad7f86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462f4423e7f37a3f4315ff16a34d2c88",
+    "url": "/listOfLaunches/index.html"
+  },
+  {
+    "revision": "c7e9d94d971cab770e0f",
+    "url": "/listOfLaunches/static/css/main.bb23f2be.chunk.css"
+  },
+  {
+    "revision": "bdfc6ca076369c51e08d",
+    "url": "/listOfLaunches/static/js/2.22bc7fe7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/listOfLaunches/static/js/2.22bc7fe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7e9d94d971cab770e0f",
+    "url": "/listOfLaunches/static/js/main.6c3e850a.chunk.js"
+  },
+  {
+    "revision": "67ea99054ae323845e18",
+    "url": "/listOfLaunches/static/js/runtime-main.351e38d5.js"
+  }
+]);
